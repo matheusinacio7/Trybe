a3c598058fcb732c454b10be6c273112856c763c
heyset <c.inaciomatheus@gmail.com>
2021-03-24 16:35:24 -0300

    Adiciona parte 4

---
+++ mod-1/bloco-04/4.1/parte4.js
@@ -0,0 +1,29 @@
+let grade = 73;
+
+if(grade >= 80) {
+  console.log("Parabéns, você foi aprovada(o)!");
+} else if (grade >= 60) {
+  console.log("Você está na nossa lista de espera.");
+} else {
+  console.log("Você foi reprovada(o)");
+}
+
+grade = 92;
+
+if(grade >= 80) {
+  console.log("Parabéns, você foi aprovada(o)!");
+} else if (grade >= 60) {
+  console.log("Você está na nossa lista de espera.");
+} else {
+  console.log("Você foi reprovada(o)");
+}
+
+grade = 41;
+
+if(grade >= 80) {
+  console.log("Parabéns, você foi aprovada(o)!");
+} else if (grade >= 60) {
+  console.log("Você está na nossa lista de espera.");
+} else {
+  console.log("Você foi reprovada(o)");
+}
