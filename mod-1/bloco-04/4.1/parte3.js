b829502f3e919e1563b3206a74f006c1adc5cf88
heyset <c.inaciomatheus@gmail.com>
2021-03-24 16:20:14 -0300

    parte3.js

---
+++ mod-1/bloco-04/4.1/parte3.js
@@ -0,0 +1,24 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(
+  typeof patientId,
+  typeof isEnrolled,
+  typeof patientInfo,
+  typeof patientEmail,
+  typeof patientAge,
+);
+
+let base = 5;
+let altura = 8;
+let area = base * altura;
+
+console.log(base, altura, area);
+
+let perimetro = altura * 2 + base * 2;
+console.log(perimetro);
