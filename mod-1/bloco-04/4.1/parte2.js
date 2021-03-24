ffc84c652340871fcea2e2f935c8caf182c9afa8
heyset <c.inaciomatheus@gmail.com>
2021-03-24 16:08:32 -0300

    adiciona parte 2

---
+++ mod-1/bloco-04/4.1/parte2.js
@@ -0,0 +1,13 @@
+const name = "Matheus Inacio";
+const birthCity = "Itumbiara, GO";
+let birthYear = 1996;
+
+console.log(name, birthCity, birthYear);
+
+birthYear = 2020;
+
+console.log(birthYear);
+
+birthCity = "nopelandia";
+
+console.log(birthCity);
