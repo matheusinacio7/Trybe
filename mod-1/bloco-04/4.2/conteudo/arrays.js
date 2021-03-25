30dc6c9ec35e1719a37a33529cc0a63d494e894a
heyset <c.inaciomatheus@gmail.com>
2021-03-25 14:54:49 -0300

    Adiciona o exercicio de conteudo 1

---
+++ mod-1/bloco-04/4.2/conteudo/arrays.js
@@ -0,0 +1,18 @@
+// exercicio 1
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+// exercicio 2
+
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+// exercicio 3
+
+menu.push('Contato');
+
+console.log(menu);
