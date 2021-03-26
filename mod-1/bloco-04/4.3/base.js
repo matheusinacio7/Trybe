c324dec5e5eb0cb453c2a56b5c587714605f6327
heyset <c.inaciomatheus@gmail.com>
2021-03-26 16:54:17 -0300

    Adiciona exercicio 4

--- mod-1/bloco-04/4.3/base.js
+++ mod-1/bloco-04/4.3/base.js
@@ -44,3 +44,25 @@ for(let i = 1; i <= n; i++) {
   }
   console.log(row);
 }
+
+
+// exercicio 4
+
+n = 5;
+
+let rows = Math.ceil(n / 2);
+
+for(let i = 0; i < rows; i++) {
+  let row = "";
+  let firstDot = rows - i;
+  let lastDot = rows + i;
+
+  for(let j = 1; j <= n; j++) {
+    if(j >= firstDot && j <= lastDot) {
+      row += "*";
+    } else {
+      row += " ";
+    }
+  }
+  console.log(row);
+}
