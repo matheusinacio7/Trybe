d968a8781f66ada9e67b7f13d25d3b800eeb2f9c
heyset <c.inaciomatheus@gmail.com>
2021-03-26 16:39:31 -0300

    Adiciona exercicios 2 e 3

--- mod-1/bloco-04/4.3/base.js
+++ mod-1/bloco-04/4.3/base.js
@@ -11,3 +11,36 @@ for(let i = 0; i < n; i++) {
 
   console.log(row);
 }
+
+
+// exercicio 2
+
+n = 5;
+
+for(let i = 0; i < n; i++) {
+  let row = "";
+
+  for(let j = 0; j < i + 1; j++) {
+    row += "*";
+  }
+
+  console.log(row);
+}
+
+
+// exercicio 3
+
+n = 5;
+
+for(let i = 1; i <= n; i++) {
+  let row = "";
+
+  for(let j = 1; j <= n; j++) {
+    if(j > (n - i)) {
+      row += "*";
+    } else {
+      row += " ";
+    }
+  }
+  console.log(row);
+}
