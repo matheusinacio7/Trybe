bc984f6d3a9021641f9e4420b06c133fe04ed30b
heyset <c.inaciomatheus@gmail.com>
2021-04-06 16:05:28 -0300

    Refatora o codigo para encapsular dentro de uma function

--- mod-1/bloco-05/5.1/conteudo/parte3.js
+++ mod-1/bloco-05/5.1/conteudo/parte3.js
@@ -1,15 +1,17 @@
 document.getElementById('header-container').style.backgroundColor = '#00B069';
 
-document.getElementsByClassName('emergency-tasks')[0].style.backgroundColor = '#FF9F84';
-let emergencyHeaders = document.querySelectorAll('.emergency-tasks h3');
-for (let emergencyHeader of emergencyHeaders) {
-  emergencyHeader.style.backgroundColor = '#A500F3';
+function changeBackgroundColors(className, cardBackgroundColor, headerBackgroundColor) {
+  let container = document.getElementsByClassName(className)[0];
+  console.log(container);
+  container.style.backgroundColor = cardBackgroundColor;
+
+  let headers = container.querySelectorAll(`.${className} h3`);
+  for (let header of headers) {
+    header.style.backgroundColor = headerBackgroundColor;
+  }
 }
 
-document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor = '#F9DB5E';
-let noEmergencyHeaders = document.querySelectorAll('.no-emergency-tasks h3');
-for (let noEmergencyHeader of noEmergencyHeaders) {
-  noEmergencyHeader.style.backgroundColor = '#232525';
-}
+changeBackgroundColors('emergency-tasks', '#FF9F84', '#A500F3');
+changeBackgroundColors('no-emergency-tasks', '#F9DB5E', '#232525');
 
 document.getElementById('footer-container').style.backgroundColor = '#003533';
