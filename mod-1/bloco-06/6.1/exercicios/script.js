bf34abc63674396eb0334ce6c76bc0700e8ef3c5
heyset <c.inaciomatheus@gmail.com>
2021-04-15 17:56:45 -0300

    Adiciona os estados brasileiros

---
+++ mod-1/bloco-06/6.1/exercicios/script.js
@@ -0,0 +1,45 @@
+const formElement = document.getElementById('resume');
+
+const brazilianStates = [
+  { name: 'Acre', acronym: 'AC',},
+  { name: 'Alagoas', acronym: 'AL',},
+  { name: 'Amapá', acronym: 'AP',},
+  { name: 'Amazonas', acronym: 'AM',},
+  { name: 'Bahia', acronym: 'BA',},
+  { name: 'Ceará', acronym: 'CE',},
+  { name: 'Distrito Federal', acronym: 'DF',},
+  { name: 'Espírito Santo', acronym: 'ES',},
+  { name: 'Goiás', acronym: 'GO',},
+  { name: 'Maranhão', acronym: 'MA',},
+  { name: 'Mato Grosso', acronym: 'MT',},
+  { name: 'Mato Grosso do Sul', acronym: 'MS',},
+  { name: 'Minas Gerais', acronym: 'MG',},
+  { name: 'Paraná', acronym: 'PA',},
+  { name: 'Paraíba', acronym: 'PB',},
+  { name: 'Pará', acronym: 'PR',},
+  { name: 'Pernambuco', acronym: 'PE',},
+  { name: 'Piauí', acronym: 'PI',},
+  { name: 'Rio de Janeiro', acronym: 'RJ',},
+  { name: 'Rio Grande do Norte', acronym: 'RN',},
+  { name: 'Rio Grande do Sul', acronym: 'RS',},
+  { name: 'Rondonia', acronym: 'RO',},
+  { name: 'Roraima', acronym: 'RR',},
+  { name: 'Santa Catarina', acronym: 'SC',},
+  { name: 'Sergipe', acronym: 'SP',},
+  { name: 'São Paulo', acronym: 'SE',},
+  { name: 'Tocantins', acronym: 'TO',},  
+];
+
+function fillStateOptions() {
+  const selectElement = document.getElementById('pi-state');
+
+  for (let state of brazilianStates) {
+    const optionHTML = 
+    `<option value="${state.acronym}">${state.name}</option>`
+    ;
+
+    selectElement.insertAdjacentHTML('beforeend', optionHTML);
+  }
+}
+
+fillStateOptions();
