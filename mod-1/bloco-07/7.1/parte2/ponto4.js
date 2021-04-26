38fb2564f3f1d2b877b46b063462fc761a5bc64a
heyset <c.inaciomatheus@gmail.com>
2021-04-26 17:35:43 -0300

    Adiciona exercicio 4.1

---
+++ mod-1/bloco-07/7.1/parte2/ponto4.js
@@ -0,0 +1,12 @@
+/*
+      ATENÇÃO
+
+  Não se preocupe se você não entender os exemplos com recursividade ou os exemplos "one-liner";
+  Normalmente não é uma boa prática esse tipo de código.
+*/
+
+// -------------- Função 1: substituir x -------------- //
+
+const getTryberPhrase = (tryberName) => 'Tryber x aqui!'.replace('x', tryberName);
+
+console.log(getTryberPhrase('Inacio'));
