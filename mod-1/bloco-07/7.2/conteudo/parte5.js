4c7399dbdc559d5532df7ed30a2ba15e7c979149
heyset <c.inaciomatheus@gmail.com>
2021-04-27 17:10:15 -0300

    Adiciona parte 5

---
+++ mod-1/bloco-07/7.2/conteudo/parte5.js
@@ -0,0 +1,12 @@
+const person = {
+  name:'Roberto',
+};
+
+const lastName = {
+  lastName: 'Silva',
+};
+
+const newPerson = Object.assign({},person,lastName);
+newPerson.name = 'Gilberto';
+console.log(newPerson);
+console.log(person);
