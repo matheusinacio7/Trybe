2c409b349a3e2af08c63ef6ad5232e4136f6176a
heyset <c.inaciomatheus@gmail.com>
2021-04-27 16:54:41 -0300

    Adiciona jeito mais bonito

--- mod-1/bloco-07/7.1/parte2/ponto4.js
+++ mod-1/bloco-07/7.1/parte2/ponto4.js
@@ -10,3 +10,21 @@
 const getTryberPhrase = (tryberName) => 'Tryber x aqui!'.replace('x', tryberName);
 
 console.log(getTryberPhrase('Inacio'));
+
+const skillArray = ['Sociabilidade', 'Criatividade', 'Estratégia', 'Node', 'React'];
+
+function getTryberAndSkills(tryberName) {
+  let rollingString = getTryberPhrase(tryberName);
+
+  rollingString += ' Minhas cinco principais habilidades são:';
+
+  const sortedSkillArray = skillArray.sort();
+
+  for (let skill of sortedSkillArray) {
+    rollingString += `\n* ${skill}`;
+  }
+
+  return rollingString;
+}
+
+console.log(getTryberAndSkills('Inacio'));
