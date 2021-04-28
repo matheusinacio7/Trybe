9445900e261b2f8d53a92a8a31c115406734e1bd
heyset <c.inaciomatheus@gmail.com>
2021-04-28 17:23:47 -0300

    Adiciona exercicio 5

---
+++ mod-1/bloco-07/7.3/exercicio5.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+
+const obj1 = {
+  title: 'My Title',
+  description: 'My Description',
+};
+
+const obj2 = {
+  description: 'My Description',
+  title: 'My Title',
+};
+
+const obj3 = {
+  title: 'My Different Title',
+  description: 'My Description',
+};
+
+// implemente seus testes aqui
+
+assert.deepStrictEqual(obj1, obj2);
+assert.notDeepStrictEqual(obj1, obj3);
+assert.notDeepStrictEqual(obj2, obj3);
