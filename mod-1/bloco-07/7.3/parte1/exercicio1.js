72548a2843c8ca9be44eaee379619367a55a0800
heyset <c.inaciomatheus@gmail.com>
2021-04-28 17:46:01 -0300

    Corrige assertion de erro no exercicio parte 1.1

--- mod-1/bloco-07/7.3/parte1/exercicio1.js
+++ mod-1/bloco-07/7.3/parte1/exercicio1.js
@@ -14,4 +14,4 @@ assert.strictEqual(sum(4, 5), 9);
 assert.strictEqual(sum(0, 0), 0);
 assert.throws(() => {
   sum(4, '5');
-}, /parameters must be numbers/);
+}, /^Error: parameters must be numbers$/);
