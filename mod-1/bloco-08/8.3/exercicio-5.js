0edc31185550d33517301c118ca7cae50630c118
heyset <c.inaciomatheus@gmail.com>
2021-05-05 16:42:44 -0300

    Adiciona exercicio 5 e troca match por test no RegEx

--- mod-1/bloco-08/8.3/exercicio-5.js
+++ mod-1/bloco-08/8.3/exercicio-5.js
@@ -64,50 +64,14 @@ const books = [
 ];
 
 const expectedResult = [
- {
-    id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
-    author: {
-      name: 'George R. R. Martin',
-      birthYear: 1948,
-    },
-    releaseYear: 1991,
-  },
-  {
-    id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
-    author: {
-      name: 'J. R. R. Tolkien',
-      birthYear: 1892,
-    },
-    releaseYear: 1954,
-  },
-  {
-    id: 3,
-    name: 'Fundação',
-    genre: 'Ficção Científica',
-    author: {
-      name: 'Isaac Asimov',
-      birthYear: 1920,
-    },
-    releaseYear: 1951,
-  },
-  {
-    id: 4,
-    name: 'Duna',
-    genre: 'Ficção Científica',
-    author: {
-      name: 'Frank Herbert',
-      birthYear: 1920,
-    },
-    releaseYear: 1965,
-  },
+  'Frank Herbert',
+  'George R. R. Martin',
+  'Isaac Asimov',
+  'J. R. R. Tolkien',
 ];
 
-function fantasyOrScienceFiction() {
-  return books.filter(book => (/(Ficção Científica)|(Fantasia)/).test(book.genre));
+function fantasyOrScienceFictionAuthors() {
+  return books.filter(book => (/(Ficção Científica)|(Fantasia)/).test(book.genre)).map(book => book.author.name).sort();
 }
 
-assert.deepStrictEqual(fantasyOrScienceFiction(), expectedResult);
+assert.deepStrictEqual(fantasyOrScienceFictionAuthors(), expectedResult);
