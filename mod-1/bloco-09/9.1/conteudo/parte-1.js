a32cb92008dac0d36c769e2cfc3bee60930e0fca
heyset <c.inaciomatheus@gmail.com>
2021-05-11 14:56:34 -0300

    Adiciona pratica de conteudo

---
+++ mod-1/bloco-09/9.1/conteudo/parte-1.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+
+const pushNumber = (list, number) => {
+  list.push(number);
+  console.log(list);
+};
+
+let numbers = [];
+
+setTimeout(() => pushNumber(numbers, 1), 3000);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+setTimeout(() => assert.deepStrictEqual(numbers, [2, 3, 1]), 3000);
