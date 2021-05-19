132697ca00e7a7a2d3b5fab7d4bd926bfd3362fc
heyset <c.inaciomatheus@gmail.com>
2021-05-19 16:07:01 -0300

    Adiciona exercicio 3

--- mod-1/bloco-10/10.2/exercicio-2-3.test.js
+++ mod-1/bloco-10/10.2/exercicio-2-3.test.js
@@ -1,16 +1,30 @@
 const { findUserById, getUserName, users } = require('./exercicio-2-3');
 
 describe('findUserById', () => {
-  test('Encontra o usuário corretamente', () => {
+  it('Encontra o usuário corretamente', () => {
     return expect(findUserById(4)).resolves.toEqual({name: 'Mark'});
   });
-  test('Retorna erro se o usuário não existe', () => {
+  it('Retorna erro se o usuário não existe', () => {
     return expect(findUserById(3)).rejects.toEqual({error: 'User with ID 3 not found.'});
   });
 });
 
 // Resolvi fazer de um jeito para findUserById e de outro para getUserName
 
-// describe('getUserName', () => {
-//   test()
-// })
+describe('getUserName', () => {
+  it('Encontra o usuário corretamente', async () => {
+    const userName = await getUserName(5);
+    expect(userName).toBe('Paul');
+  });
+
+  it('Retorna erro se o usuário não existe', async () => {
+    try {
+      await getUserName(3);
+      fail();
+    } catch (err) {
+      expect(err).toEqual({error: 'User with ID 3 not found.'});
+    }
+  });
+});
+
+
