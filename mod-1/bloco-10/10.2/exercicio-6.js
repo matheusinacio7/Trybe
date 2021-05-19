7d1c1b88eb4f96fce29f7f7da2e549bd4d5ca5d6
heyset <c.inaciomatheus@gmail.com>
2021-05-19 16:43:02 -0300

    Adiciona exercicio 6.1

---
+++ mod-1/bloco-10/10.2/exercicio-6.js
@@ -0,0 +1,44 @@
+const Animals = [
+  { name: 'Dorminhoco', age: 1, type: 'Dog' },
+  { name: 'Soneca', age: 2, type: 'Dog' },
+  { name: 'Preguiça', age: 5, type: 'Cat' },
+];
+
+const findAnimalByName = (name) => (
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const foundAnimal = Animals.find((animal) => animal.name === name);
+
+      if (!foundAnimal) {
+        return reject('Nenhum animal com esse nome!');
+      }
+
+      return resolve(foundAnimal);
+    }, 100);
+  })
+);
+
+const getAnimal = (name) => (
+  findAnimalByName(name).then(animal => animal)
+);
+
+const findAnimalsByType = (type) => (
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const arrayAnimals = Animals.filter((animal) => animal.type === type);
+      if (arrayAnimals.length !== 0) {
+        return resolve(arrayAnimals);
+      };
+
+      return reject({ error: 'Não possui esse tipo de animal.' });
+    }, 100);
+  })
+);
+
+const getListAnimals = (type) => (
+  findAnimalsByType(type).then(list => list)
+);
+
+module.exports = {
+  getAnimal,
+}
