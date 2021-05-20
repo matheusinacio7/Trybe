bdd0dd9dbd1cc4b5f258deb0a907780b78da925d
heyset <c.inaciomatheus@gmail.com>
2021-05-20 17:09:58 -0300

    Adiciona exercicio de fixacao 4

--- mod-1/bloco-10/10.3/conteudo/math.test.js
+++ mod-1/bloco-10/10.3/conteudo/math.test.js
@@ -35,9 +35,23 @@ describe('fn somar', () => {
 
   it('É chamada com os parâmetros corretos', () => {
     expect(somar).toHaveBeenCalledWith(4, 5);
-  })
+  });
 });
 
 //4. Faça o mock da função dividir e implemente um retorno padrão com o valor '15'. Implemente também os seguintes valores para a primeira e segunda chamadas: '2' e '5'. Teste a chamada, o retorno, os parâmetros e quantas vezes a função foi chamada.
+describe('fn dividr', () => {
+  dividir = jest.fn().mockReturnValue('15');
+
+  it('Sempre retorna a string "15" (??)', () => {
+    expect(dividir('2')).toBe('15');
+    expect(dividir('5')).toBe('15');
+  });
+
+  it('Foi chamada duas vezes', () => {
+    expect(dividir).toHaveBeenCalledWith('2');
+    expect(dividir).toHaveBeenCalledWith('5');
+    expect(dividir).toHaveBeenCalledTimes(2);
+  });
+});
 
 //5. Faça o mock da função subtrair de maneira que seja possível restaurar sua implementação original. Defina como retorno padrão o valor '20'. Teste o número de chamadas e o retorno. Restaure a implementação original da função e teste sua execução.
