33dcea2df87cccd0a1c51dbd3763321376df6f66
heyset <c.inaciomatheus@gmail.com>
2021-05-20 16:54:07 -0300

    Adiciona o exercicio de fixacao 3

--- mod-1/bloco-10/10.3/conteudo/math.test.js
+++ mod-1/bloco-10/10.3/conteudo/math.test.js
@@ -1,10 +1,10 @@
-const { describe, test, expect  } = require('@jest/globals');
+const { describe, it, expect  } = require('@jest/globals');
 
 let { dividir, multiplicar, somar, subtrair } = require('./math');
 
 //1. Faça o mock da funcão subtrair e teste sua chamada.
 describe('fn subtrair', () => {
-  test('É chamada corretamente', () => {
+  it('É chamada corretamente', () => {
     subtrair = jest.fn();
     subtrair();
 
@@ -14,14 +14,29 @@ describe('fn subtrair', () => {
 
 //2. Faça o mock da função multiplicar e implemente como retorno padrão o valor '10'. Teste a chamada e o retorno.
 describe('fn multiplicar', () => {
-  test('Retorna 10', () => {
-    multiplicar = jest.fn().mockReturnValue(10);
+  multiplicar = jest.fn().mockReturnValue(10);
+
+  it('Retorna 10', () => {
     expect(multiplicar()).toBe(10);
+  });
+
+  it('É chamada corretamente', () => {
     expect(multiplicar).toHaveBeenCalled();
   })
 })
 
 //3. Faça o mock da função somar e implemente uma função que recebe dois valores e retorna sua soma. Teste a chamada, o retorno e os parâmetros passados.
+describe('fn somar', () => {
+  somar = jest.fn((a, b) => a + b);
+
+  it('Faz a soma corretamente', () => {
+    expect(somar(4, 5)).toBe(9);
+  });
+
+  it('É chamada com os parâmetros corretos', () => {
+    expect(somar).toHaveBeenCalledWith(4, 5);
+  })
+});
 
 //4. Faça o mock da função dividir e implemente um retorno padrão com o valor '15'. Implemente também os seguintes valores para a primeira e segunda chamadas: '2' e '5'. Teste a chamada, o retorno, os parâmetros e quantas vezes a função foi chamada.
 
