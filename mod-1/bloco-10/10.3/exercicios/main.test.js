1bc98999ebee6e0712ca9b1167425d7e47313f40
heyset <c.inaciomatheus@gmail.com>
2021-05-21 15:44:26 -0300

    Adiciona exercicio 3

--- mod-1/bloco-10/10.3/exercicios/main.test.js
+++ mod-1/bloco-10/10.3/exercicios/main.test.js
@@ -14,4 +14,17 @@ describe('A função getRandomIntUpTo100', () => {
     expect(getRandomIntUpTo100(28, 7)).toBe(4);
     expect(getRandomIntUpTo100()).toBe(10);
   });
+
+  it('Retorna a multiplicação de três parâmetros', () => {
+    getRandomIntUpTo100 = getRandomIntUpTo100.mockImplementation((a, b, c) => a * b * c);
+    expect(getRandomIntUpTo100(3, 2, 5)).toBe(30);
+    expect(getRandomIntUpTo100(4, 4, 2)).toBe(32);
+  });
+
+  it('Retorna o dobro de um parâmetro', () => {
+    getRandomIntUpTo100.mockReset();
+    getRandomIntUpTo100 = getRandomIntUpTo100.mockImplementation((num) => num * 2);
+    expect(getRandomIntUpTo100(10)).toBe(20);
+    expect(getRandomIntUpTo100(35)).toBe(70);
+  })
 });
