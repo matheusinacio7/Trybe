87f101eb7a0ff3a12c12c878ed81296c4845068c
heyset <c.inaciomatheus@gmail.com>
2021-05-24 15:02:17 -0300

    Adiciona modificacoes

--- demos/mock/main.test.js
+++ demos/mock/main.test.js
@@ -3,18 +3,6 @@ const { describe, it, expect } = require('@jest/globals');
 const { getGreetingString } = require('./main.js');
 const { getAll, findOne, insertOne, removeOne, updateOne } = require('./bancoDeDadosMaisLentoDoMundo');
 
-describe('A função getGreetingString', () => {
-  describe('Quando o usuário é encontrado', () => {
-    const expectedString = `Olá! Meu nome é Janete Corça e minha comida favorita é Sushi!
-Qual a sua??`
-
-    it('Retorna a frase correta', () => {
-      return findOne((user) => user.name === 'Janete Corça')
-        .then((user) => {
-          expect(getGreetingString('Olá', user)).toBe(expectedString);
-        });
-    });
-  });
-});
-
-
+// describe('A função getGreetingString', () => {
+//   it('Retorna a re')
+// });
