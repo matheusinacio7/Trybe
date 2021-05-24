026e9617c91ae0fe85ddedaf65d9996e45894074
heyset <c.inaciomatheus@gmail.com>
2021-05-24 16:48:30 -0300

    Adiciona exercicio de conteudo parte8

---
+++ mod-2/bloco-11/11.1/conteudo/parte8-classes-e-react/src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import HelloThere from './components/HelloThere';
+
+function App() {
+  return <HelloThere />;
+}
+
+export default App;
---
+++ mod-2/bloco-11/11.1/conteudo/parte8-classes-e-react/src/components/HelloThere.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class HelloThere extends React.Component {
+  render() {
+    return (
+    <React.Fragment>
+      <h1>Hello, React! Eu sou o Inacio</h1>
+      <p>Eu sou noob no LoR ainda :/</p>
+    </React.Fragment>
+    );
+  }
+}
+
+export default HelloThere;
