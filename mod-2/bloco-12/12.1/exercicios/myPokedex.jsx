2875fe3fadc0e1cc1c6a8cdc544422712e471623
heyset <c.inaciomatheus@gmail.com>
2021-05-28 17:09:16 -0300

    Adiciona estilos

--- mod-2/bloco-12/12.1/exercicios/myPokedex.jsx
+++ mod-2/bloco-12/12.1/exercicios/myPokedex.jsx
@@ -152,14 +152,16 @@ class Controls extends React.Component {
 
   render() {
     return (
-      <section>
+      <section className='controls'>
         <div>
           <Button onClick={this.props.handlers.next} disabled={!this.props.hasNext}>Próximo</Button>
         </div>
         <div>
           <h1>Filtrar Por Tipo</h1>
-          {this.props.typeList.map((type) => <Button key={`${type}-filter`} onClick={() => this.props.handlers.filter(type)} >{type}</Button>)}
-          <Button onClick={() => this.props.handlers.filter('any')}>Todos</Button>
+          <ul className='controls__filters'>
+            {this.props.typeList.map((type) => <li key={`${type}-filter`}><Button onClick={() => this.props.handlers.filter(type)} >{type}</Button></li>)}
+            <li key='any-filter'><Button onClick={() => this.props.handlers.filter('any')}>Todos</Button></li>
+          </ul>
         </div>
       </section>
     );
