37138c7c230969886443b598020a82eda8021c4a
heyset <c.inaciomatheus@gmail.com>
2021-07-07 17:51:31 -0300

    nsei

--- mod-2/bloco-16/16.3/my-resume/src/utils/withStore.js
+++ mod-2/bloco-16/16.3/my-resume/src/utils/withStore.js
@@ -1,6 +1,5 @@
 import { createStore } from "redux";
 import { connect } from "react-redux";
-
 import { rootReducer } from "../reducers";
 
 export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
