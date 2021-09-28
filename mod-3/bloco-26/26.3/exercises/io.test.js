e82682b421772d40c4b3706b461e1f90a0a7309a
heyset <c.inaciomatheus@gmail.com>
2021-09-28 16:23:30 -0300

    Adiciona teste exercicio 4

---
+++ mod-3/bloco-26/26.3/exercises/io.test.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const fsp = require('fs/promises');
+
+const { describe, it } = require('mocha');
+const { expect } = require('chai');
+const { stub } = require('sinon');
+
+const { writeContentToFile } = require('./io.js');
+
+describe('A função writeContentToFile', () => {
+  it('escreve corretamente o conteúdo para um arquivo destino', async () => {
+    const targetFileName = 'sinon.txt';
+    const targetContent = 'A lightning bolt is pretty much the best weapon ever.';
+    const spies = [];
+    spies.push(stub(fs, 'writeFile').returns(undefined));
+    spies.push(stub(fs, 'writeFileSync').returns(undefined));
+    spies.push(stub(fsp, 'writeFile').returns(undefined));
+    spies[0].calledWithMatch(['', '', ''])
+
+    await writeContentToFile(targetFileName, targetContent);
+
+    expect(true).to.satisfy(() => {
+      for (let i = 0; i < spies.length; i += 1) {
+        if (spies[i].calledWithMatch(`io/${targetFileName}`), targetContent) {
+          return true
+        }
+      }
+      return false
+    });
+  });
+});
