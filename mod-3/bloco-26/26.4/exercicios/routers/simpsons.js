aa49475748bd524440807caf6fa1a7cf825a0759
heyset <c.inaciomatheus@gmail.com>
2021-09-29 22:41:42 -0300

    Adiciona exercicio de encontrar simpson por id

--- mod-3/bloco-26/26.4/exercicios/routers/simpsons.js
+++ mod-3/bloco-26/26.4/exercicios/routers/simpsons.js
@@ -1,9 +1,28 @@
 const fs = require('fs/promises');
 const router = require('express').Router();
 
+const getFile = () => fs.readFile('db/simpsons.json', 'utf-8').then((data) => JSON.parse(data));
+
+const saveFile = (data) => fs.writeFile('db/simpsons.json', JSON.stringify(data, null, '\t'));
+
 router.get('/', (_, res) => {
-  fs.readFile('db/simpsons.json', 'utf-8')
-    .then((data) => res.status(200).json(JSON.parse(data)))
+  getFile()
+    .then((data) => res.status(200).json(data))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).end();
+    });
+});
+
+router.get('/:id', (req, res) => {
+  getFile()
+    .then((data) => {
+      const simpson = data.find(({id}) => id == req.params.id);
+
+      if (!simpson) return res.status(404).json({ message: 'Simpson not found' });
+
+      return res.status(200).json(simpson);
+    })
     .catch((err) => {
       console.log(err);
       res.status(500).end();
