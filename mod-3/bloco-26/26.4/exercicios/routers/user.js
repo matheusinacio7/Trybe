b01b66ca4523faeacd0c287c880b6678aa3ba048
heyset <c.inaciomatheus@gmail.com>
2021-09-30 01:37:12 -0300

    Adiciona error handling basico no middleware

--- mod-3/bloco-26/26.4/exercicios/routers/user.js
+++ mod-3/bloco-26/26.4/exercicios/routers/user.js
@@ -1,7 +1,6 @@
 const fs = require('fs/promises');
 const { promisify } = require('util');
 const randomBytesAsync = promisify(require('crypto').randomBytes);
-const crypto = require('crypto');
 const bcrypt = require('bcrypt');
 const SALT_ROUNDS = 10;
 
@@ -14,14 +13,19 @@ const saveUsers = (data) => fs.writeFile('db/users.json', JSON.stringify(data, n
 router.post('/signin', (req, res, next) => {
   const { email, password } = req.body;
 
-  if (!email || !password) return res.status(401).json({ message: 'Missing password or email field' });
+  if (!email || !password) {
+    return next(new Error({
+      status: 401,
+      reason: 'Missing password or email field'
+    }));
+  }
 
   getAllUsers()
     .then((users) => {
       const userIndex = users.findIndex((user) => user.email === email);
 
       if (userIndex === -1) {
-        throw new Error({ status: 403, message: null });
+        throw new Error({ status: 403, reason: null });
       };
 
       return Promise.all([
@@ -33,7 +37,7 @@ router.post('/signin', (req, res, next) => {
     })
     .then(([passwordMatches, users, userIndex, token]) => {
       if (!passwordMatches) {
-        throw new Error({ status: 403, message: null });
+        throw new Error({ status: 403, reason: null });
       }
 
       users[userIndex].tokens.push(token);
@@ -47,10 +51,12 @@ router.post('/signin', (req, res, next) => {
     .catch(next);
 });
 
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
   const { email, password, firstName, phone } = req.body;
 
-  if (!email || !password || !firstName || !phone) return res.status(401).json({ message: 'Missing fields' });
+  if (!email || !password || !firstName || !phone) {
+    return next(new Error({ status: 401, reason: 'Missing fields' }));
+  }
 
   Promise.all([
     bcrypt.hash(password, SALT_ROUNDS),
@@ -67,17 +73,20 @@ router.post('/', (req, res) => {
 
       const existingUser = allUsers.find((user) => user.email === email);
 
-      if (existingUser) return res.status(400).send({ message: 'This email is already in use.' });
+      if (existingUser) {
+        throw new Error({ status: 400, reason: 'This email is already in use.' });
+      }
 
       const newUsersArray = [...allUsers, user];
 
       return saveUsers(newUsersArray);
     })
     .then(() => res.status(201).send({ message: 'User created successfully' }))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).end();
-    });
+    .catch(next);
+});
+
+router.use((err, req, res, next) => {
+  return res.status(401).end();
 });
 
 module.exports = router;
