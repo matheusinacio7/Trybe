b2703584666a48c2788cc10c26986b118095598c
heyset <c.inaciomatheus@gmail.com>
2021-10-07 16:56:52 -0300

    Adiciona rota que rtorna todos os livros

--- mod-3/bloco-26-v2/26-v2.1/conteudo/model-with-mongodb/app/models/Book.js
+++ mod-3/bloco-26-v2/26-v2.1/conteudo/model-with-mongodb/app/models/Book.js
@@ -40,20 +40,34 @@ class Book {
 
   static getAll() {
     return new Promise((resolve, reject) => {
-      connection.execute(
-        `
-          SELECT
-            b.title,
-            CONCAT(a.first_name, ' ', a.middle_name, ' ', a.last_name) AS author
-          FROM
-            books AS b
-          INNER JOIN
-            authors AS a
-            ON a.id = b.author_id
-        `
-      )
-        .then(([rows]) => {
-          resolve(rows);
+      connection()
+        .then((db) => {
+          return db.collection('books').aggregate([
+            {
+              $lookup: {
+                from: 'authors',
+                localField: 'author_id',
+                foreignField: '_id',
+                as: 'authors'
+              },
+            },
+            {
+              $project: {
+                _id: 0,
+                title: 1,
+                author: { $first: '$authors' }
+              }
+            },
+            {
+              $project: {
+                title: 1,
+                author: { $concat: ['$author.firstName', ' ', '$author.middleName', ' ','$author.lastName'] }
+              }
+            }
+          ]).toArray()
+        })
+        .then((books) => {
+          resolve(books);
         })
         .catch(reject);
     });
