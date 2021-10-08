b3acc200e549e10362564698be7102636b3c948f
heyset <c.inaciomatheus@gmail.com>
2021-10-08 16:24:48 -0300

    Adiciona connect

--- mod-3/bloco-26-v2/26-v2.1/exercicios/mongodb/index.js
+++ mod-3/bloco-26-v2/26-v2.1/exercicios/mongodb/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import userRouter from './src/routers/user.js';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/user', userRouter);
 
 app.listen(process.env.PORT, () => {
   console.log('Server up on port', process.env.PORT);
