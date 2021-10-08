4fc905ff4680923dbb4375f2104c9b6c98ba8961
heyset <c.inaciomatheus@gmail.com>
2021-10-08 17:22:06 -0300

    Adiciona error true

--- mod-3/bloco-26-v2/26-v2.1/exercicios/mongodb/src/middlewares/handleError.js
+++ mod-3/bloco-26-v2/26-v2.1/exercicios/mongodb/src/middlewares/handleError.js
@@ -4,15 +4,15 @@ import HttpError from '../errors/HttpError.js';
 export default function handleError(err, _req, res, _next) {
   if (err instanceof InternalError) {
     console.log(err);
-    return res.status(err.status).json({ message: err.message });
+    return res.status(err.status).json({ error: true, message: err.message });
   }
 
   if (err instanceof HttpError) {
-    return res.status(err.status).json({ message: err.message });
+    return res.status(err.status).json({ error: true, message: err.message });
   }
 
   const error = new Error('Erro não capturado pelo fluxo.');
   error.reason = err;
   console.log(err);
-  res.status(500).json({ message: 'Erro interno no servidor' });
+  res.status(500).json({ error: true, message: 'Erro interno no servidor' });
 }
