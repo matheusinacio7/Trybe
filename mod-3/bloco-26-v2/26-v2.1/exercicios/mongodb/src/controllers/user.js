3ba9fc1d949451bf23978750c66884de2c0c3e86
heyset <c.inaciomatheus@gmail.com>
2021-10-08 17:14:56 -0300

    Adiciona bcrypt para hashing de senha

--- mod-3/bloco-26-v2/26-v2.1/exercicios/mongodb/src/controllers/user.js
+++ mod-3/bloco-26-v2/26-v2.1/exercicios/mongodb/src/controllers/user.js
@@ -1,10 +1,17 @@
 import User from '../models/User.js';
+import bcrypt from 'bcrypt';
 import validate from '../validator/validate.js';
+
+const SALT_ROUNDS = 10;
 
 export const createNewUser = (userData) => new Promise((resolve, reject) => {
   validate('user', userData);
 
-  User.createNew(userData)
-    .then(resolve)
+  bcrypt.hash(userData.password, SALT_ROUNDS)
+    .then((hashedPassword) => {
+      return User.createNew({ ...userData, password: hashedPassword })
+        .then(resolve)
+        .catch(reject);
+    })
     .catch(reject);
 });
