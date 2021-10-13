10b33b7d2c21870920f91d6eb7bbcdfcc0e89657
heyset <c.inaciomatheus@gmail.com>
2021-10-13 16:25:28 -0300

    Adiciona status e modifica urls para serem RESTful

--- mod-3/bloco-26-v2/26-v2.3/refactor/controllers/productController.js
+++ mod-3/bloco-26-v2/26-v2.3/refactor/controllers/productController.js
@@ -3,38 +3,50 @@ const ProductModel = require('../models/productModel');
 
 const router = express.Router();
 
-router.get('/list-products', async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   const products = await ProductModel.getAll();
 
-  res.send(products);
+  res.status(200).json(products);
 });
 
-router.get('/get-by-id/:id', async (req, res, next) => {
-  const product = await ProductModel.getById(req.params.id);
-
-  res.send(product);
-});
-
-router.post('/add-user', async (req, res) => {
+router.post('/', async (req, res) => {
   const { name, brand } = req.body;
 
   const newProduct = await ProductModel.add(name, brand);
 
-  res.send(newProduct);
+  res.status(201).json(newProduct);
 });
 
-router.post('/delete-user/:id', async (req, res) => {
+router.get('/:id', async (req, res, next) => {
+  const product = await ProductModel.getById(req.params.id);
+
+  if (!product) {
+    return res.status(404).end();
+  }
+
+  res.status(200).json(product);
+});
+
+router.delete('/:id', async (req, res) => {
   const products = await ProductModel.exclude(req.params.id);
 
-  res.send(products);
+  if (!Object.keys(products).length) {
+    return res.status(404).end();
+  }
+
+  res.status(200).json(products);
 });
 
-router.post('/update-user/:id', async (req, res) => {
+router.put('/:id', async (req, res) => {
   const { name, brand } = req.body;
 
   const products = await ProductModel.update(req.params.id, name, brand);
 
-  res.send(products);
+  res.status(200).json(products);
+});
+
+router.use('/', (_req, res) => {
+  res.status(405).end();
 });
 
 module.exports = router;
