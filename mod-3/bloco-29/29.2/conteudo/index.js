2d6dd9e5d883287313a520c44cd112ca8d4e9f55
heyset <c.inaciomatheus@gmail.com>
2021-11-25 17:54:30 -0300

    Adiciona dep de express e modifica para nao usar bodyParser

--- mod-3/bloco-29/29.2/conteudo/index.js
+++ mod-3/bloco-29/29.2/conteudo/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const bodyParser = require("body-parser");
 
 const userController = require('./controllers/userController');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use('/user', userController);
 
