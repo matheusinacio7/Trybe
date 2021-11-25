d3d085b72ceec07de36e7168081404f83418e6c6
heyset <c.inaciomatheus@gmail.com>
2021-11-25 19:17:51 -0300

    Adiciona model e migration de book

---
+++ mod-3/bloco-29/29.2/exercicios/my-book-library/src/migrations/20211125221614-create-book.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      author: {
+        type: Sequelize.STRING
+      },
+      pageQuantity: {
+        type: Sequelize.INTEGER,
+        field: 'page_quantity',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'created_at',
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'updated_at',
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Books');
+  }
+};
