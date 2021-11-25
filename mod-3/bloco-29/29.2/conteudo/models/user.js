f8496ba14dbeba85819c4f0f46284ff63e53c87f
heyset <c.inaciomatheus@gmail.com>
2021-11-25 17:32:56 -0300

    Adiciona phone num no model

--- mod-3/bloco-29/29.2/conteudo/models/user.js
+++ mod-3/bloco-29/29.2/conteudo/models/user.js
@@ -2,8 +2,8 @@ const User = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     fullName: DataTypes.STRING,
     email: DataTypes.STRING,
+    phone_num: DataTypes.STRING,
   });
-
 
   return User;
 }
