a4191890fee48b85f866e5683f91bfb7964455ee
heyset <c.inaciomatheus@gmail.com>
2021-12-05 19:35:13 -0300

    Adiciona setup basico socket

---
+++ mod-3/bloco-30/30.3/conteudo/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000', // url aceita pelo cors
+    methods: ['GET', 'POST'], // Métodos aceitos pela url
+  }});
+
+io.on('connection', (socket) => {
+  console.log(`Usuário conectado. ID: ${socket.id}`);
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+});
