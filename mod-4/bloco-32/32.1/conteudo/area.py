f0a9ebe81457719219cb86a834af0bcded8b77ff
heyset <c.inaciomatheus@gmail.com>
2022-01-20 16:59:12 -0300

    adiciona mudancas (sim eu sei)

--- mod-4/bloco-32/32.1/conteudo/area.py
+++ mod-4/bloco-32/32.1/conteudo/area.py
@@ -16,6 +16,7 @@ def circle(radius):
     return PI * radius * radius
 
 
-print("Área do quadrado:", square(10))
-print("Área do retângulo:", rectangle(2, 2))
-print("Área do círculo:", circle(3))
+if __name__ == '__main__':
+    print("Área do quadrado:", square(10))
+    print("Área do retângulo:", rectangle(2, 2))
+    print("Área do círculo:", circle(3))
