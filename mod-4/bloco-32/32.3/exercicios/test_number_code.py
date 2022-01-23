ac0e78e7fc5043ef29637a925075d1eaf60d090d
heyset <c.inaciomatheus@gmail.com>
2022-01-23 20:12:26 -0300

    adiciona decode de codigo numerico

--- mod-4/bloco-32/32.3/exercicios/test_number_code.py
+++ mod-4/bloco-32/32.3/exercicios/test_number_code.py
@@ -5,12 +5,12 @@ from pytest import raises
 def test_decode_throws_error_with_invalid_characters():
     invalid_phrase_1 = '1-OB&*-PLEASE'
     with raises(TypeError,
-                match='must only use letters, numbers 0 and 1 and hifens'):
+                match='must only use letters, numbers 0 and 1 and hifen'):
         decode(invalid_phrase_1)
 
     invalid_phrase_2 = 'WHAT IS MY NAME'
     with raises(TypeError,
-                match='must only use letters, numbers 0 and 1 and hifens'):
+                match='must only use letters, numbers 0 and 1 and hifen'):
         decode(invalid_phrase_2)
 
     invalid_phrase_3 = ''
@@ -22,9 +22,9 @@ def test_decode_returns_decoded_phrase():
     valid_phrase_1 = '1-HOME-SWEET-HOME'
     decoded_phrase_1 = '1-4663-79338-4663'
 
-    assert(decode(valid_phrase_1)) is decoded_phrase_1
+    assert(decode(valid_phrase_1)) == decoded_phrase_1
 
     valid_phrase_2 = 'MY-MISERABLE-JOB'
     decoded_phrase_2 = '69-647372253-562'
 
-    assert(decode(valid_phrase_2)) is decoded_phrase_2
+    assert(decode(valid_phrase_2)) == decoded_phrase_2
