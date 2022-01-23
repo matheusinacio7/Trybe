ca4ddf26031e50fc4a5afc35a6335dcc6130bf58
heyset <c.inaciomatheus@gmail.com>
2022-01-23 19:36:20 -0300

    Adiciona fizz buzz

--- mod-4/bloco-32/32.3/exercicios/test_fizz_buzz.py
+++ mod-4/bloco-32/32.3/exercicios/test_fizz_buzz.py
@@ -3,12 +3,13 @@ from fizz_buzz import get_up_to_n
 
 def test_get_up_to_n_substitutes_correctly():
     fizz_buzz_list = get_up_to_n(31)
+
     for i in range(1, 32):
         if (i % 3 == 0 and i % 5 == 0):
-            assert(fizz_buzz_list[i] == 'FizzBuzz')
-        elif (i % 3):
-            assert(fizz_buzz_list[i] == 'Fizz')
-        elif (i % 5):
-            assert(fizz_buzz_list[i] == 'Buzz')
+            assert(fizz_buzz_list[i - 1] == 'FizzBuzz')
+        elif (i % 3 == 0):
+            assert(fizz_buzz_list[i - 1] == 'Fizz')
+        elif (i % 5 == 0):
+            assert(fizz_buzz_list[i - 1] == 'Buzz')
         else:
-            assert(fizz_buzz_list[i] == i)
+            assert(fizz_buzz_list[i - 1] == i)
