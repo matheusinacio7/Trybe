08a71445e41be7e3fbf69526cd9e4dc7594bdbdd
heyset <c.inaciomatheus@gmail.com>
2022-01-23 04:58:04 -0300

    Adiciona validacao para nomes pequenos

--- mod-4/bloco-32/32.2/exercicios/whos_this_pokemon_game.py
+++ mod-4/bloco-32/32.2/exercicios/whos_this_pokemon_game.py
@@ -33,7 +33,7 @@ def main_loop():
     scrambled_indexes = sample(indexes, len(indexes))
     revealed_indexes = []
 
-    for chance in range(5, 0, -1):
+    for chance in range(len(name) - 3 if len(name) > 3 else 1, 0, -1):
         print(f'You have {chance} chance{"s" if chance > 1 else ""} left\n')
         revealed = substitute_with_underscores_except(name, revealed_indexes)
         print(f'Pokemon {number}:   {get_with_spaces(revealed)}\n')
