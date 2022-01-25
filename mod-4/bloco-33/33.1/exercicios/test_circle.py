7d7bf9f3568ebc2432c8b90e48890b7d9b860d19
heyset <c.inaciomatheus@gmail.com>
2022-01-25 16:15:22 -0300

    Adiciona testes de recalculo

--- mod-4/bloco-33/33.1/exercicios/test_circle.py
+++ mod-4/bloco-33/33.1/exercicios/test_circle.py
@@ -10,3 +10,21 @@ def test_has_correct_attributes():
     assert approx(my_circle.area, 0.1) == 113.10
     assert approx(my_circle.perimeter, 0.1) == 37.7
     assert approx(my_circle.circumference, 0.1) == 37.7
+
+
+def test_attributes_get_recalculated():
+    my_circle = Circle(radius=6)
+
+    assert(my_circle.radius) == 6
+    assert(my_circle.diameter) == 12
+    assert approx(my_circle.area, 0.1) == 113.10
+    assert approx(my_circle.perimeter, 0.1) == 37.7
+    assert approx(my_circle.circumference, 0.1) == 37.7
+
+    my_circle.radius = 10
+
+    assert(my_circle.radius) == 10
+    assert(my_circle.diameter) == 20
+    assert approx(my_circle.area, 0.1) == 314.16
+    assert approx(my_circle.perimeter, 0.1) == 62.83
+    assert approx(my_circle.circumference, 0.1) == 62.83
