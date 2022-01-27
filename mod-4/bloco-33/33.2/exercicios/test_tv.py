dc9f0b3466a99f9ea20da707b85953e3c7061461
heyset <c.inaciomatheus@gmail.com>
2022-01-27 20:03:41 -0300

    adiciona teste para increase volume

--- mod-4/bloco-33/33.2/exercicios/test_tv.py
+++ mod-4/bloco-33/33.2/exercicios/test_tv.py
@@ -1,10 +1,21 @@
+from pytest import fixture
 from tv import TV
 
+@fixture
+def my_tv():
+    return TV('24 inches')
 
-def test_initalizes_with_correct_values():
-    my_tv = TV('24 inches')
 
+def test_initalizes_with_correct_values(my_tv):
     assert(my_tv.volume) == 50
     assert(my_tv.channel) == 1
     assert(my_tv.size) == '24 inches'
     assert(my_tv.isOn) is False
+
+
+def test_increases_volume(my_tv):
+    assert(my_tv.volume) == 50
+
+    my_tv.increase_volume()
+
+    assert(my_tv.volume) == 51
