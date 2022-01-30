8b783bd52a55b9fff1cb43c63cd3d93e4cfb2b1f
heyset <c.inaciomatheus@gmail.com>
2022-01-30 02:55:49 -0300

    geometry: feat: adiciona rectangle

--- mod-4/bloco-33/33.2/exercicios/geometric_figures/test_rectangle.py
+++ mod-4/bloco-33/33.2/exercicios/geometric_figures/test_rectangle.py
@@ -3,10 +3,10 @@ from geometric_figures.rectangle import Rectangle
 
 
 def test_has_correct_perimeter():
-    my_rectangle = Rectangle(height=10, widht=6)
+    my_rectangle = Rectangle(height=10, width=6)
     assert(my_rectangle.perimeter) == approx(32)
 
 
 def test_has_correct_area():
-    my_rectangle = Rectangle(height=10, widht=6)
+    my_rectangle = Rectangle(height=10, width=6)
     assert(my_rectangle.area) == approx(60)
