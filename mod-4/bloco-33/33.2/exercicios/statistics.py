08a19787eec7a7cd0c1002eab52525df9d33bafc
heyset <c.inaciomatheus@gmail.com>
2022-01-30 01:36:49 -0300

    statistics: test: adiciona base da interface

---
+++ mod-4/bloco-33/33.2/exercicios/statistics.py
@@ -0,0 +1,12 @@
+class Statistics:
+    def __init__(initial_list=[]):
+        raise NotImplementedError
+
+    def getMean():
+        raise NotImplementedError
+
+    def getMedian():
+        raise NotImplementedError
+
+    def getMode():
+        raise NotImplementedError
