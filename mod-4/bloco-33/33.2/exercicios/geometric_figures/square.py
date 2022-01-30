edee7c12c22d788776e7f88a33d9cd06ce747cd5
heyset <c.inaciomatheus@gmail.com>
2022-01-30 03:01:22 -0300

    geometry: feat: adiciona square

---
+++ mod-4/bloco-33/33.2/exercicios/geometric_figures/square.py
@@ -0,0 +1,14 @@
+from geometric_figures.geometric_figure import GeometricFigure
+
+
+class Square(GeometricFigure):
+    def __init__(self, side: float) -> None:
+        self.side = side
+
+    @property
+    def area(self) -> float:
+        return self.side ** 2
+
+    @property
+    def perimeter(self) -> float:
+        return self.side * 4
