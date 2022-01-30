39c3cb6230b2376fd2c9995857a393bd1175622a
heyset <c.inaciomatheus@gmail.com>
2022-01-30 01:49:17 -0300

    statistics: feat: adiciona funcionalidades estatisticas

--- mod-4/bloco-33/33.2/exercicios/my_statistics.py
+++ mod-4/bloco-33/33.2/exercicios/my_statistics.py
@@ -1,12 +1,15 @@
+from statistics import mean, median, mode
+
+
 class Statistics:
-    def __init__(initial_list=[]):
-        raise NotImplementedError
+    def __init__(self, initial_list=[]):
+        self._number_list = initial_list
 
-    def getMean():
-        raise NotImplementedError
+    def getMean(self):
+        return mean(self._number_list)
 
-    def getMedian():
-        raise NotImplementedError
+    def getMedian(self):
+        return median(self._number_list)
 
-    def getMode():
-        raise NotImplementedError
+    def getMode(self):
+        return mode(self._number_list)
