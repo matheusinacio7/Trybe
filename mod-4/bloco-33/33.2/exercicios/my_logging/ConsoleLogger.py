9c7d80b1fe67958ef37a53bfd26bae118937eb4a
heyset <c.inaciomatheus@gmail.com>
2022-01-31 19:13:06 -0300

    nsei

--- mod-4/bloco-33/33.2/exercicios/my_logging/ConsoleLogger.py
+++ mod-4/bloco-33/33.2/exercicios/my_logging/ConsoleLogger.py
@@ -1,6 +1,11 @@
-from .Logger import Logger
+from my_logging.Logger import Logger
 
 
 class ConsoleLogger(Logger):
     def log(self, message: str) -> None:
         print(message)
+
+
+my_logger = ConsoleLogger()
+
+my_logger.log('opa')
