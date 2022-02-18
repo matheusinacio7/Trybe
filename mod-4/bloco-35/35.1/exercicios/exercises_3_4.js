8135ae940dc97232cb233f94c1371733812ef0b1
heyset <c.inaciomatheus@gmail.com>
2022-02-18 16:48:51 -0300

    adiciona um monte de exercicio ai

---
+++ mod-4/bloco-35/35.1/exercicios/exercises_3_4.js
@@ -0,0 +1,15 @@
+// Exercício 3 O código abaixo está em JavaScript . Calcule sua ordem de complexidade para um array de tamanho n .
+
+// const numbers = [0,1,2,3,4,5,6,7,8,9]
+// numbers.map(n => n*n)
+
+// Complexidade de tempo: O(n). Espaço: O(n)
+
+//  Exercício 4 O código abaixo está em JavaScript . Calcule sua ordem de complexidade para um array de tamanho n . 
+
+// const numbers = [0,1,2,3,4,5,6,7,8,9]
+// numbers.map(n => n*n)
+// .filter(n => n%2 === 0)
+// .reduce((acc, n) => acc + n)
+
+// Complexidade de tempo: O(n^3). Espaço: O(n)
