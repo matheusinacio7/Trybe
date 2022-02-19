bb6b551953c795029dac42564064c0dae3422aa6
heyset <c.inaciomatheus@gmail.com>
2022-02-18 21:32:32 -0300

    refatora console renderer

--- mod-4/bloco-35/35.2/Hanoi/ConsoleRenderer.py
+++ mod-4/bloco-35/35.2/Hanoi/ConsoleRenderer.py
@@ -19,6 +19,34 @@ class ConsoleRenderer(Renderer):
         if (self.should_clear):
             self.clear()
 
+        self._render_message()
+
+        max_width = tower.number_of_disks
+        top_to_bottom_lines = range(max_width - 1, -1, -1)
+
+        for n in top_to_bottom_lines:
+            self._write_line(tower=tower, line_num=n, max_width=max_width)
+
+        self.console.write('\n')
+        sleep(1.5)
+
+    def _render_message(self):
+        self.console.write(self.current_message + '\n\n')
+
+    def _write_line(self,
+                    tower: HanoiTower,
+                    line_num: int,
+                    max_width: int) -> None:
+        spacing = '    '
+
+        self.console.write(spacing)
+        for peg in self._get_pegs(tower):
+            self._render_peg(peg, line_num, max_width)
+            self.console.write(spacing)
+
+        self.console.write('\n')
+
+    def _get_pegs(self, tower):
         max_width = tower.number_of_disks
         A: List[int] = list(tower.A['list'])
         B: List[int] = list(tower.B['list'])
@@ -27,26 +55,18 @@ class ConsoleRenderer(Renderer):
         A.extend([0] * (max_width - len(A)))
         B.extend([0] * (max_width - len(B)))
         C.extend([0] * (max_width - len(C)))
+        return [A, B, C]
 
-        self.console.write(self.current_message + '\n\n')
-        spacing = '    '
-        peg = '**'
+    def _render_peg(self, peg: List[int], line_num: int, max_width):
+        peg_tokens = '**'
         peg_space = ' ' * (max_width - 1)
-        for n in range(max_width - 1, -1, -1):
-            self.console.write(spacing)
-            for filled_peg in [A, B, C]:
-                if filled_peg[n] == 0:
-                    self.console.write(peg_space + peg + peg_space)
-                else:
-                    disk = '##' * filled_peg[n]
-                    empty_space = ' ' * (max_width - filled_peg[n])
-                    self.console.write(empty_space + disk + empty_space)
-                self.console.write(spacing)
 
-            self.console.write('\n')
+        if peg[line_num] == 0:
+            self.console.write(peg_space + peg_tokens + peg_space)
+        else:
+            disk = '##' * peg[line_num]
+            empty_space = ' ' * (max_width - peg[line_num])
+            self.console.write(empty_space + disk + empty_space)
 
-        self.console.write('\n')
-        sleep(1.5)
-
-    def clear(self) -> None:
+    def _clear(self) -> None:
         os.system('cls' if os.name == 'nt' else 'clear')
